e9c24b424556e14b8d33cd3a661846f83ded7b1e
liuzhihao <wb-lzh760330@alibaba-inc.com>
2021-08-19 17:24:37 +0800

    chore: 搭建富文本基础框架

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // 箭头函数只有一个参数的时候可以忽略括号
+  arrowParens: 'avoid',
+  // 括号内部不要出现空格
+  bracketSpacing: true,
+  // 行结束符使用 Unix 格式
+  endOfLine: 'lf',
+  // true: Put > on the last line instead of at a new line
+  jsxBracketSameLine: false,
+  parser: 'typescript',
+}
